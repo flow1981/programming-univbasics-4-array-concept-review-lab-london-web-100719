f76063dd0f1d518a9cb0efff087ed0bd2bb547ec
Florian Stagliano <contact.f.stagliano@gmail.com>
2019-09-30 13:05:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,12 +11,14 @@ end
 
 def find_max_value(array)
   
+  max=nil;
+  
   array.length.times { |index|
-  if array[index]==value_to_find
-    return index
+  if array[index]>max
+    max=array[index]
   end
   }
-  
+  return max
 end
 
 def find_min_value(array)
