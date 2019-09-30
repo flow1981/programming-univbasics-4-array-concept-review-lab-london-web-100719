f09d45aa2facec464efabf4a9961da7631b710fa
Florian Stagliano <contact.f.stagliano@gmail.com>
2019-09-30 13:03:26 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,12 +1,11 @@
 def find_element_index(array, value_to_find)
   
   array.length.times { |index|
-  
   if array[index]==value_to_find
     return index
   end
-    
   }
+  return
 end
 
 def find_max_value(array)
